fdb932bdbc6fa83632ec4c3f1d57774f3d4af260
Sergej Herbert <herbert.sergej@gmail.com>
2022-06-28 20:57:19 +0200

    feat: Allow using legacy code editor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) 
 
 ## [Unreleased]
 
-## [3.1.0] - 2022-06-22
+## [3.1.0] - 2022-06-28
 
 ### Changed
 
--- selenium/writing_queries.test.ts
+++ selenium/writing_queries.test.ts
@@ -24,16 +24,44 @@ describe.only('writing queries', () => {
     await driver.quit();
   });
 
-  it.only(
+  it(
     'runs an updated query',
     saveTestState(testStatus, async () => {
-      // the .inputarea element is an invisible accessibility element belonging to the monaco
-      // code editor
+      // the .inputarea element is an invisible accessibility element belonging to the monaco code editor
       await driver.findElement(By.css('.inputarea')).sendKeys(Key.chord(Key.CONTROL, 'a'), 'SELECT 12345678987654321');
       await driver.findElement(By.css('.explore-toolbar')).click();
+
+      // check that the query was executed with the new input
       await driver.wait(
         until.elementLocated(
           By.xpath(`//div[contains(@aria-label, 'Explore Table')]//*[text()[contains(., "12345678987654321")]]`)
+        ),
+        5 * 1000
+      );
+    })
+  );
+
+  it(
+    'converts the new code editor to the legacy code editor',
+    saveTestState(testStatus, async () => {
+      // the .inputarea element is an invisible accessibility element belonging to the monaco code editor
+      await driver.findElement(By.css('.inputarea')).sendKeys(Key.chord(Key.CONTROL, 'a'), 'SELECT 12121992');
+      await driver.findElement(By.xpath(`//input[contains(@role, 'use-legacy-editor-switch')]//..//label`)).click();
+      await driver.wait(
+        until.elementLocated(
+          By.xpath(`//textarea[contains(@role, 'query-editor-input')][text()[contains(., "12121992")]]`)
+        ),
+        5 * 1000
+      );
+
+      await driver
+        .findElement(By.css('[role="query-editor-input"]'))
+        .sendKeys(Key.chord(Key.CONTROL, 'a'), 'SELECT 231992');
+
+      // check that the query was executed with the new input
+      await driver.wait(
+        until.elementLocated(
+          By.xpath(`//div[contains(@aria-label, 'Explore Table')]//*[text()[contains(., "231992")]]`)
         ),
         5 * 1000
       );
